c586896c38cd70e3eae27cc787bd2b9789566ce8
Zhanarys <37359098+Zhanarys27@users.noreply.github.com>
2023-07-21 12:36:45 +0600

    add all file

---
+++ main.py
@@ -0,0 +1,36 @@
+#TASK 1
+# n = 125
+# res = (n%10)+((n%100)//10)+(n//100)
+# print(res)
+
+#TASK2
+# n = 24
+# s = n//3
+# print(s//2, s*2, s//2)
+
+
+
+#TASK 3
+# n = 123341
+# levaya = n//1000
+# pravaya = n%1000
+#
+# levaya = (levaya%10)+((levaya%100)//10)+(levaya//100)
+# pravaya = (pravaya%10)+((pravaya%100)//10)+(pravaya//100)
+# if levaya == pravaya:
+#     print("yes")
+# else:
+#     print("no")
+
+#TASK 4
+# a, b, c = 3,5,13
+# res = (a*b)
+# if res>= c and (res%c) !=0:
+#     print("yes")
+# elif c==b and c == a:
+#     print("yes")
+# else:
+#     print("no")
+
+
+
